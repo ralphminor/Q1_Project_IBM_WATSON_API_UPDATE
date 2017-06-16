a48b0308e91f5d55014eef0bd00632806625197a
ralphminor <ralphminor@users.noreply.github.com>
2017-06-15 18:30:07 -0600

    Completed routes in index.js including /analysis route that hits the watson natural language understanding api via the watson-developer-cloud npm package.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+require('dotenv').load();
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const nlu = new NaturalLanguageUnderstandingV1({
+  'username': process.env.username,
+  'password': process.env.password,
+  'version_date': '2017-02-27'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/analysis', function(req, res, next) {
+  var parameters = {
+    'url': req.body.url,
+    'features': {
+      'emotion': {}
+    }
+  }
+
+
+  nlu.analyze(parameters, function(err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      res.send(response)
+  });
+
 });
 
 module.exports = router;
